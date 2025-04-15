ad84fe6a38822e5d463be81aee387701c1f94711
alina464 <chudnetsovaalina@gmail.com>
2025-04-15 15:29:31 +0200

    hw27

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,38 @@
 (() => {
 	const refs = {
-	  openModalBtn: document.querySelector("[data-modal-open]"),
-	  closeModalBtn: document.querySelector("[data-modal-close]"),
+	  openModalBtn: document.querySelector("[data-open-modal]"),
+	  closeModalBtn: document.querySelector("[data-close-modal]"),
 	  modal: document.querySelector("[data-modal]"),
+	  modalBackdrop: document.querySelector("[data-modal]"),
 	};
+
+	refs.modal.addEventListener("transitionend", () => {
+		if (!refs.modal.classList.contains("show")) {
+		  refs.modalBackdrop.classList.add("is-hidden");
+		}
+	  });
   
-	refs.openModalBtn.addEventListener("click", toggleModal);
-	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openModalBtn.addEventListener("click", openModal);
+	refs.closeModalBtn.addEventListener("click", closeModal);
   
-	function toggleModal() {
-	  refs.modal.classList.toggle("is-hidden");
-	  document.body.classList.toggle("no-scroll");
+	function openModal() {
+	  refs.modal.classList.remove("is-hidden");
+  
+	  // Трішки почекати і додати клас show, щоб анімація спрацювала
+	  requestAnimationFrame(() => {
+		refs.modal.classList.add("show");
+	  });
+  
+	  document.body.classList.add("no-scroll");
+	}
+  
+	function closeModal() {
+	  refs.modal.classList.remove("show");
+  
+	  // Почекати завершення анімації і сховати модалку
+	  setTimeout(() => {
+		refs.modal.classList.add("is-hidden");
+	  }, 500); // має співпадати з transition
+	  document.body.classList.remove("no-scroll");
 	}
   })();
